91ea17145a691e7d04c3ff7d70f95bc8d2d0ebe6
Ariel Moatti <github@arielmoatti.com>
2020-10-15 16:09:14 +0200

    laptop sync

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const db = require("./db");
 
 app.use(express.static("./public"));
 
@@ -8,7 +9,30 @@ app.get("/", (req, res) => {
     res.end();
 });
 
-//
+app.get("/actors", (req, res) => {
+    db.getActors()
+        .then(({ rows }) => {
+            console.log("results:", rows);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    res.end("check your terminal...");
+});
+
+app.post("/add-actor", (req, res) => {
+    db.addActor("Ariel Moatti", 43)
+        .then(() => {
+            //worked!
+            console.log("added city!");
+            res.end("thank you!");
+        })
+        .catch((err) => {
+            //err
+            console.log("error in add-actor", err);
+        });
+});
+
 //
 //
 //
