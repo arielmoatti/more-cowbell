9daa6b6f218a64010f6f6e590e31ada1420b03dc
arielmoatti <github@arielmoatti.com>
2020-10-19 00:16:31 +0200

    PART2 v3, better canvas

--- petition.sql
+++ petition.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS signatures;
 
 CREATE TABLE signatures (
-    id SERIAL PRIMARY KEY,
-    first VARCHAR NOT NULL CHECK (first != ''),
-    last VARCHAR NOT NULL CHECK (last != ''),
-    signature VARCHAR NOT NULL CHECK (signature != '')
+    id SERIAL       PRIMARY KEY,
+    userid          VARCHAR,
+    first           VARCHAR NOT NULL CHECK (first != ''),
+    last            VARCHAR NOT NULL CHECK (last != ''),
+    signature       VARCHAR NOT NULL CHECK (signature != ''),
+    created         TIMESTAMP DEFAULT NOW()
 );
-
---INSERT INTO signatures (first, last, signature) VALUES ('Ariel-Test', 'Moatti-Test', 'blank-Test');
--- public/script.js
+++ public/script.js
@@ -1,29 +1,50 @@
-// console.log("jquery is on", $);
 const canvas = document.getElementById("canvas");
+let x,
+    y,
+    drawPing = null;
+
 if (canvas) {
     const context = canvas.getContext("2d");
     const signCanvas = document.querySelector('input[name="signature"]');
 
     canvas.addEventListener("mousedown", (e) => {
-        let x = e.clientX - canvas.offsetLeft;
-        let y = e.clientY - canvas.offsetTop;
-        console.log("click x", x);
-        console.log("click y", y);
+        x = e.clientX - canvas.offsetLeft;
+        y = e.clientY - canvas.offsetTop;
+
+        drawStart();
+
+        if (!drawPing) {
+            drawPing = setInterval(draw, 1);
+        }
+    });
+
+    canvas.addEventListener("mousemove", (e) => {
+        x = e.clientX - canvas.offsetLeft;
+        y = e.clientY - canvas.offsetTop;
+    });
+
+    canvas.addEventListener("mouseup", () => {
+        drawEnd();
+    });
+
+    drawStart = () => {
+        context.strokeStyle = "cornflowerblue";
+        context.lineCap = "round";
+        context.lineJoin = "round";
+
         context.beginPath();
         context.moveTo(x, y);
+    };
 
-        canvas.addEventListener("mousemove", function draw(e) {
-            let x = e.clientX - canvas.offsetLeft;
-            let y = e.clientY - canvas.offsetTop;
-            console.log("position x", x);
-            console.log("position y", y);
-            context.lineTo(x, y);
-            context.stroke();
+    draw = () => {
+        context.lineTo(x, y);
+        context.stroke();
+    };
 
-            canvas.addEventListener("mouseup", () => {
-                canvas.removeEventListener("mousemove", draw);
-                signCanvas.value = canvas.toDataURL();
-            });
-        });
-    });
+    drawEnd = () => {
+        clearInterval(drawPing);
+        drawPing = null;
+        context.closePath();
+        signCanvas.value = canvas.toDataURL();
+    };
 }
