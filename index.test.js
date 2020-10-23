eee1ca8c8ff2f8b5524da3a2f569d173d7e82971
arielmoatti <github@arielmoatti.com>
2020-10-23 20:29:03 +0200

    SUBMIT Jest exercise

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const { app } = require("./index.js");
 // jest automatically looks in our mock folder for 'cookie-session'
 const cookieSession = require("cookie-session");
 
-test("1. user logout: GET /petition redirects to /register", () => {
+test("1. logged-out user: GET /petition redirects to /register", () => {
     cookieSession.mockSessionOnce();
     return supertest(app)
         .get("/petition")
@@ -13,7 +13,7 @@ test("1. user logout: GET /petition redirects to /register", () => {
         });
 });
 
-test("2a. user login: GET /register redirects to /petition", () => {
+test("2a. logged user: GET /register redirects to /petition", () => {
     cookieSession.mockSessionOnce({
         userId: 1,
     });
@@ -25,7 +25,7 @@ test("2a. user login: GET /register redirects to /petition", () => {
         });
 });
 
-test("2b. user login: GET /petition redirects to /thank-you", () => {
+test("2b. logged user: GET /petition redirects to /thank-you", () => {
     cookieSession.mockSessionOnce({
         userId: 1,
     });
@@ -37,7 +37,7 @@ test("2b. user login: GET /petition redirects to /thank-you", () => {
         });
 });
 
-test.only("3. user login, signed: GET /petition redirects to /thank-you", () => {
+test("3. logged user, signed: GET /petition redirects to /thank-you", () => {
     cookieSession.mockSessionOnce({
         userId: 1,
         signed: true,
@@ -50,15 +50,26 @@ test.only("3. user login, signed: GET /petition redirects to /thank-you", () => 
         });
 });
 
-test.only("3. user login, signed: GET /petition redirects to /thank-you", () => {
+test("4a. logged user, not signed: GET /thank-you redirects to /petition", () => {
     cookieSession.mockSessionOnce({
         userId: 1,
-        signed: true,
     });
     return supertest(app)
-        .get("/petition")
+        .get("/thank-you")
         .then((res) => {
-            expect(res.headers.location).toBe("/thank-you");
+            expect(res.headers.location).toBe("/petition");
+            expect(res.statusCode).toBe(302);
+        });
+});
+
+test("4b. logged user, not signed: GET /signerslist redirects to /petition", () => {
+    cookieSession.mockSessionOnce({
+        userId: 1,
+    });
+    return supertest(app)
+        .get("/signerslist")
+        .then((res) => {
+            expect(res.headers.location).toBe("/petition");
             expect(res.statusCode).toBe(302);
         });
 });
